f207c8261f4fcdc1aa3380b68404ebc3e69258d0
geffest <rascel@list.ru>
2021-02-10 00:16:06 +0700

    start commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+   <AppHeader></AppHeader>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+export default {
+  name: 'App',
+
+  components: {
+  AppHeader,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
